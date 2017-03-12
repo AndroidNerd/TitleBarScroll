630b8e9cd9657da31fc1d975c846cfe40503e97c
nerd <wuqingpizinie@gmail.com>
2017-03-12 16:15:11 +0800

    沉浸式测试

--- app/src/main/java/com/example/nerd/rx_retrofit_okhttp/MainActivity.java
+++ app/src/main/java/com/example/nerd/rx_retrofit_okhttp/MainActivity.java
@@ -2,9 +2,9 @@ package com.example.nerd.rx_retrofit_okhttp;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.support.v4.widget.NestedScrollView;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,12 +19,12 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.OnClick;
 
-public class MainActivity extends BaseActivity implements View.OnScrollChangeListener{
+public class MainActivity extends BaseActivity implements NestedScrollView.OnScrollChangeListener {
     @BindView(R.id.banner)
     FlyBanner banner;
 
     @BindView(R.id.scrollView)
-    ScrollView scrollView;
+    NestedScrollView scrollView;
 
     @BindView(R.id.ll_head)
     LinearLayout ll_head;
@@ -40,13 +40,13 @@ public class MainActivity extends BaseActivity implements View.OnScrollChangeLis
 
     @Override
     protected void initSet() {
-        Drawable scanDrawable=getDrawable(R.drawable.scan);
-        scanDrawable.setBounds(0,0,40,40);
-        tv_scan.setCompoundDrawables(null,scanDrawable,null,null);
+        Drawable scanDrawable = getResources().getDrawable(R.drawable.scan);
+        scanDrawable.setBounds(0, 0, 40, 40);
+        tv_scan.setCompoundDrawables(null, scanDrawable, null, null);
 
-        Drawable gitDrawable=getDrawable(R.drawable.gift);
-        gitDrawable.setBounds(0,0,40,40);
-        tv_gift.setCompoundDrawables(null,gitDrawable,null,null);
+        Drawable gitDrawable = getResources().getDrawable(R.drawable.gift);
+        gitDrawable.setBounds(0, 0, 40, 40);
+        tv_gift.setCompoundDrawables(null, gitDrawable, null, null);
 
         ll_head.getBackground().setAlpha(0);
         scrollView.setOnScrollChangeListener(this);
@@ -60,13 +60,12 @@ public class MainActivity extends BaseActivity implements View.OnScrollChangeLis
         List<String> imgesUrl = new ArrayList<>();
         imgesUrl.add("http://e.hiphotos.baidu.com/news/w%3D638/sign=4905f0747af082022d92923c73fafb8a/f9198618367adab4aea30f1482d4b31c8701e472.jpg");
         imgesUrl.add("https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1681876004,4078626976&fm=23&gp=0.jpg");
-        imgesUrl.add("https://image.baidu.com/search/detail?ct=503316480&z=&tn=baiduimagedetail&ipn=d&word=%E5%88%98%E6%B6%9B%E8%BD%AE%E6%92%AD%E5%9B%BE&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=-1&cs=1591682781,3548446950&os=3700920313,2021961178&simid=4236578991,744995290&pn=10&rn=1&di=194940847640&ln=1253&fr=&fmq=1488964280541_R&ic=0&s=undefined&se=&sme=&tab=0&width=&height=&face=undefined&is=0,0&istype=2&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&objurl=http%3A%2F%2Fdynamic-image.yesky.com%2F1080x-%2FuploadImages%2F2015%2F336%2F18%2F65O40287011Z.jpg");
         imgesUrl.add("http://h.hiphotos.baidu.com/news/crop%3D0%2C1%2C321%2C192%3Bw%3D638/sign=a39c027772cb0a46916dd1795653da15/91ef76c6a7efce1b32792847a651f3deb58f65e9.jpg");
         banner.setImagesUrl(imgesUrl);
         banner.setOnItemClickListener(new FlyBanner.OnItemClickListener() {
             @Override
             public void onItemClick(int position) {
-                Toast.makeText(MainActivity.this,""+position,Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -85,10 +84,11 @@ public class MainActivity extends BaseActivity implements View.OnScrollChangeLis
     }
 
     @Override
-    public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-        if(scrollY<255){
-            ll_head.getBackground().setAlpha(scrollY);
-        }else{
+    public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+        if (scrollY < ll_head.getHeight()) {
+            float rate = (float) scrollY / ll_head.getHeight();
+            ll_head.getBackground().setAlpha((int) (rate * 256));
+        } else {
             ll_head.getBackground().setAlpha(255);
         }
     }
